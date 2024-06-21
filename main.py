54981ed4bf60071cd01465a41c268a8b34e9f937
Corso Hashtag <110099413+corsohashtag@users.noreply.github.com>
2024-06-21 08:06:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+from pyscript import document, display
+from random import shuffle
+
+
+def set_answer_buttons(enabled):
+    for i in (1, 2, 3):
+        target = select("#button-answer" + str(i))
+        target.disabled = not enabled
+
+
+def answer_button_click(event):
+    user_choice = event.srcElement  # Sorgente dell'evento, es, "button-answer1"
+    set_answer_buttons(enabled=False)
+
+    if user_choice.innerText == to_guess:  # Controlla la scelta dell'utente
+        message = "Risposta esatta!"
+        icon_filename = "./images/true.svg"
+    else:
+        message = f"Risposta sbagliata! La risposta esatta era {to_guess}"
+        icon_filename = "./images/false.svg"
+
+    select("#message-text").innerText = message
+    select("#message-img").src = icon_filename
+    select("#message-container").style.display = "block"
+
+
+def play(event=None):
+    global nations, to_guess
+
+    select("#message-container").style.display = "none"
+
+    shuffle(nations)  # Mescola tutte le nazioni
+    answers = nations[:3]  # Prende le prime tre nazioni come possibili risposte
+    to_guess = answers[0]  # Sceglie la prima come nazione estratta
+    to_guess_filename = f"./images/flags/{to_guess.replace(' ', '_')}.png"
+    select("#flag-image").src = to_guess_filename
+    shuffle(answers)  # Mescola le tre risposte
+    set_answer_buttons(enabled=True)  # Abilita tutti i pulsanti di risposta
+
+    for i, nation in enumerate(answers):
+        select("#button-answer" + str(i + 1)).innerText = nation
+
+    select("#loading-button").style.display = "none"
+    select("#main-container").style.display = "block"
+
+
+nations = open("nations.csv", encoding="utf-8").readlines()
+to_guess = None  # Nome della nazione da indovinare
+select = document.querySelector  # Alias della funzione che seleziona elemento/i HTML
+play()
